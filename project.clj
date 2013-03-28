db9286a5a616034aa823aa3429bd21ef40a482c3
Andrew Mcveigh <me@andrewmcveigh.com>
2013-03-28 17:15:47 +0100

    Proof of concept

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject com.andrewmcveigh/lein-boot "0.1.0-SNAPSHOT"
+(defproject com.andrewmcveigh/lein-boot "0.1.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0"]
-                 [ring/ring-servlet "1.1.8" :exclusions [javax.servlet/servlet-api]]]
-  :profiles {:dev {:dependencies [[org.eclipse.jetty/jetty-webapp "8.1.10.v20130312"]]}}
+  :dependencies [[ring/ring-servlet "1.1.8" :exclusions [javax.servlet/servlet-api]]
+                 [org.eclipse.jetty/jetty-webapp "8.1.10.v20130312"]]
   :eval-in-leiningen true)
--- src/leiningen/boot.clj
+++ src/leiningen/boot.clj
@@ -1,9 +1,9 @@
 (ns leiningen.boot
   (:require 
+    [clojure.pprint]
     [leiningen.repl :as repl]
     [ring.util.servlet :as servlet]
     [clojure.java.io :as io]) 
-  (:gen-class :extends javax.servlet.http.HttpServlet) 
   (:import
     [org.eclipse.jetty.server Server Request]
     [org.eclipse.jetty.server.handler AbstractHandler]
@@ -14,46 +14,45 @@
     [org.eclipse.jetty.servlet ServletHolder]
     [javax.servlet.http HttpServletRequest HttpServletResponse]
     [org.eclipse.jetty.util.resource Resource]
-    [org.eclipse.jetty.webapp WebAppContext WebAppClassLoader])) 
-
-
-(def server (atom nil))
+    [org.eclipse.jetty.webapp WebAppContext WebAppClassLoader]))
 
 (defn find-webapp-root []
   (cond (.exists (io/as-file "src/test/webapp")) "src/test/webapp"
         (.exists (io/as-file "src/main/webapp")) "src/main/webapp"
         (.exists (io/as-file "public")) "public"))
 
-(defn boot-server
-  ([handler port]
-   (let [path (find-webapp-root)
-         context (WebAppContext. path "/")
-         cloader (WebAppClassLoader. context)]
-     (doseq [x (seq (.getURLs (java.lang.ClassLoader/getSystemClassLoader)))
-             :let [file (io/as-file x)
-                   resource (Resource/newResource file)
-                   filename (.getName file)]
-             :when (.endsWith filename ".jar")]
-       (.addJars cloader resource))
-     (.setClassLoader context cloader)
-     (when-not @server (reset! server (Server. port)))
-     (doto @server
-       (.stop)
-       (.setHandler (doto context
-                      (.addServlet
-                        (ServletHolder.
-                          (proxy [javax.servlet.http.HttpServlet] []
-                            (service [request response]
-                              ((ring.util.servlet/make-service-method (resolve handler))
-                               this request response))))
-                        "/*")))
-       (.start))))
-  ([handler] (boot-server handler 8080)))
-
 (defn boot [project & args]
   (let [handler (or (:handler (:ring project)) (:boot project))
+        port 8080
+        server (atom nil)
+        boot-server `(do
+                       (require 'ring.util.servlet)
+                       ;(prn ~(symbol (namespace handler)))
+                       (require '~(symbol (namespace handler)))
+                       (let [path# ~(find-webapp-root)
+                           context# (WebAppContext. path# "/")
+                           cloader# (WebAppClassLoader. context#)]
+                       (doseq [x# (seq (.getURLs (java.lang.ClassLoader/getSystemClassLoader)))
+                               :let [file# (io/as-file x#)
+                                     resource# (Resource/newResource file#)
+                                     filename# (.getName file#)]
+                               :when (.endsWith filename# ".jar")]
+                         (.addJars cloader# resource#))
+                       (.setClassLoader context# cloader#)
+                       ;(when-not (deref ~server) (reset! ~server (Server. ~port)))
+                       (doto (Server. ~port) 
+                         (.stop)
+                         (.setHandler (doto context#
+                                        (.addServlet
+                                          (ServletHolder.
+                                            (proxy [javax.servlet.http.HttpServlet] []
+                                              (service [request# response#]
+                                                ((servlet/make-service-method (resolve '~handler))
+                                                 ~'this request# response#))))
+                                          "/*")))
+                         (.start))))
+        _ (do (clojure.pprint/pprint boot-server) (prn))
         project (update-in project
                            [:repl-options :init]
-                           #(list 'do %
-                                  (list `boot-server handler)))]
+                           #(list 'do % boot-server))]
     (repl/repl project)))
