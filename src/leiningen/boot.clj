988f81647ee22bc2ac4b6f9a2a0f458472651558
Andrew Mcveigh <me@andrewmcveigh.com>
2013-03-29 14:33:02 +0100

    Working with meta-inf/resources

--- src/leiningen/boot.clj
+++ src/leiningen/boot.clj
@@ -1,9 +1,10 @@
 (ns leiningen.boot
-  (:require 
+  (:require
     [clojure.pprint]
     [leiningen.repl :as repl]
     [ring.util.servlet :as servlet]
-    [clojure.java.io :as io]) 
+    [clojure.java.io :as io]
+    [clojure.string :as string])
   (:import
     [org.eclipse.jetty.server Server Request]
     [org.eclipse.jetty.server.handler AbstractHandler]
@@ -14,45 +15,74 @@
     [org.eclipse.jetty.servlet ServletHolder]
     [javax.servlet.http HttpServletRequest HttpServletResponse]
     [org.eclipse.jetty.util.resource Resource]
-    [org.eclipse.jetty.webapp WebAppContext WebAppClassLoader]))
+    [org.eclipse.jetty.webapp Configuration WebAppContext WebAppClassLoader
+     WebInfConfiguration WebXmlConfiguration MetaInfConfiguration FragmentConfiguration
+     JettyWebXmlConfiguration TagLibConfiguration]))
 
 (defn find-webapp-root []
   (cond (.exists (io/as-file "src/test/webapp")) "src/test/webapp"
         (.exists (io/as-file "src/main/webapp")) "src/main/webapp"
         (.exists (io/as-file "public")) "public"))
 
+(defn boot-server [port handler]
+  `(do
+     (require 'ring.util.servlet)
+     (require '~(symbol (namespace handler)))
+     (def ~'ring-server (atom nil))
+     (defn ~'start-server []
+       (let [path# ~(find-webapp-root)
+             context# (WebAppContext. path# "/")
+             cloader# (WebAppClassLoader. context#)
+             meta-conf# (MetaInfConfiguration.)]
+         (.setConfigurationDiscovered context# true)
+         (doseq [x# (seq (.getURLs (java.lang.ClassLoader/getSystemClassLoader)))
+                 :let [file# (io/as-file x#)
+                       resource# (Resource/newResource file#)
+                       filename# (.getName file#)]
+                 :when (.endsWith filename# ".jar")]
+           (let [meta-inf-resource# (Resource/newResource
+                                      (str "jar:file:"
+                                           (.getCanonicalPath file#)
+                                           "!/META-INF/resources"))]
+             (when (.exists meta-inf-resource#)
+               (.addResource meta-conf#
+                             context#
+                             WebInfConfiguration/RESOURCE_URLS
+                             meta-inf-resource#)))
+           (.addJars cloader# resource#))
+         (.setConfigurations
+           context#
+           (into-array Configuration
+                       [(WebInfConfiguration.)
+                        (WebXmlConfiguration.)
+                        meta-conf#
+                        (FragmentConfiguration.)
+                        (JettyWebXmlConfiguration.)]))
+         (.setClassLoader context# cloader#)
+         (when-not @~'ring-server (reset! ~'ring-server (Server. ~port)))
+         (doto @~'ring-server
+           (.stop)
+           (.setHandler (doto context#
+                          (.addServlet
+                            (ServletHolder.
+                              (proxy [javax.servlet.http.HttpServlet] []
+                                (service [request# response#]
+                                  ((servlet/make-service-method (resolve '~handler))
+                                   ~'this request# response#))))
+                            "/*")))
+           (.start))))
+     (defn ~'stop-server [] (.stop @~'ring-server))
+     (~'start-server)))
+
 (defn boot [project & args]
-  (let [handler (or (:handler (:ring project)) (:boot project))
-        port 8080
-        server (atom nil)
-        boot-server `(do
-                       (require 'ring.util.servlet)
-                       ;(prn ~(symbol (namespace handler)))
-                       (require '~(symbol (namespace handler)))
-                       (let [path# ~(find-webapp-root)
-                           context# (WebAppContext. path# "/")
-                           cloader# (WebAppClassLoader. context#)]
-                       (doseq [x# (seq (.getURLs (java.lang.ClassLoader/getSystemClassLoader)))
-                               :let [file# (io/as-file x#)
-                                     resource# (Resource/newResource file#)
-                                     filename# (.getName file#)]
-                               :when (.endsWith filename# ".jar")]
-                         (.addJars cloader# resource#))
-                       (.setClassLoader context# cloader#)
-                       ;(when-not (deref ~server) (reset! ~server (Server. ~port)))
-                       (doto (Server. ~port) 
-                         (.stop)
-                         (.setHandler (doto context#
-                                        (.addServlet
-                                          (ServletHolder.
-                                            (proxy [javax.servlet.http.HttpServlet] []
-                                              (service [request# response#]
-                                                ((servlet/make-service-method (resolve '~handler))
-                                                 ~'this request# response#))))
-                                          "/*")))
-                         (.start))))
-        _ (do (clojure.pprint/pprint boot-server) (prn))
+  (let [{:keys [port]}
+        (apply hash-map
+               (mapcat (fn [[k v]] [(keyword (string/replace k #"^:" "")) v])
+                       (partition 2 args)))
+        port (try (Integer. port) (catch Exception _))
+        port (or port (:port (:ring project)) 8080)
+        handler (or (:handler (:ring project)) (:boot project))
         project (update-in project
                            [:repl-options :init]
-                           #(list 'do % boot-server))]
+                           #(list 'do % (boot-server port handler)))]
     (repl/repl project)))
