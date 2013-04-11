f336d51ee571e05346170cf3c9b01ad3ff0b35e9
Andrew Mcveigh <me@andrewmcveigh.com>
2013-04-11 17:26:43 +0200

    Start adding default servlet

--- src/leiningen/boot.clj
+++ src/leiningen/boot.clj
@@ -22,13 +22,16 @@
      MetaInfConfiguration FragmentConfiguration JettyWebXmlConfiguration
      TagLibConfiguration]))
 
-(defn find-webapp-root []
-  (cond (.exists (io/as-file "src/test/webapp")) "src/test/webapp"
-        (.exists (io/as-file "src/main/webapp")) "src/main/webapp"
-        (.exists (io/as-file "public")) "public"))
-
 (defn war-resources-path [project]
   (:war-resources-path project "war-resources"))
+
+(defn find-webapp-root [project]
+  (let [war-resources (war-resources-path project)]
+    (if (.exists (io/as-file war-resources))
+      war-resources
+      (cond (.exists (io/as-file "src/test/webapp")) "src/test/webapp"
+            (.exists (io/as-file "src/main/webapp")) "src/main/webapp"
+            (.exists (io/as-file "public")) "public"))))
 
 (def web-app-ignore
   #{"/WEB-INF" "/META-INF" "/.DS_Store"})
@@ -55,6 +58,7 @@
 
 (def add-servlet-mappings
   '(defn add-servlet-mappings [context & mappings]
+     (prn (.getServlet (.getServletHandler context) "default"))
      (doseq [mapping mappings]
        (.addServletMapping (.getServletHandler context) mapping))))
 
@@ -98,7 +102,7 @@
         ;MetaInfConfiguration FragmentConfiguration JettyWebXmlConfiguration
         ;TagLibConfiguration])))
 
-(defn boot-server [port default-mappings handlers]
+(defn boot-server [webapp-root port default-mappings handlers]
   `(do
      ;~namespace-decl
      (ns ~'boot)
@@ -111,7 +115,7 @@
      ~add-servlet-mappings
      (def ~'ring-server (atom nil))
      (defn ~'start-server []
-       (let [path# ~(find-webapp-root)
+       (let [path# ~webapp-root
              context# (WebAppContext. path# "/")
              cloader# (WebAppClassLoader. context#)
              meta-conf# (MetaInfConfiguration.)]
@@ -135,11 +139,10 @@
            context#
            (into-array Configuration
                        [(WebInfConfiguration.)
-                        ;(WebXmlConfiguration.)
+                        (WebXmlConfiguration.)
                         meta-conf#
                         (FragmentConfiguration.)
                         (JettyWebXmlConfiguration.)]))
-         (~'add-servlet-mappings (~'->default-servlet-mapping ~default-mappings))
          (.setClassLoader context# cloader#)
          (when-not @~'ring-server (reset! ~'ring-server (Server. ~port)))
          (doseq [handler# ~(mapv (fn [x] `(var ~x)) handlers)
@@ -150,6 +153,10 @@
                  (servlet/servlet handler#))
                (str (when (and ~(> (count handlers) 1) ctx#)
                       (str \/ ctx#)) "/*"))))
+         (.addServlet (.getServletHandler context#)
+                      (ServletHolder.
+                        (org.eclipse.jetty.servlet.DefaultServlet.)))
+         (~'add-servlet-mappings context# (~'->default-servlet-mapping ~default-mappings))
          (doto @~'ring-server
            (.stop)
            (.setHandler context#)
@@ -175,14 +182,20 @@
         handlers (or (:handler (:ring project)) (:boot project))
         handlers (if (sequential? handlers) handlers [handlers])
         mappings (servlet-mappings project)
-        _ (clojure.pprint/pprint (boot-server port mappings handlers))
+        _ (clojure.pprint/pprint (boot-server (find-webapp-root project)
+                                              port
+                                              mappings
+                                              handlers))
         project (update-project project
                                 deps/add-if-missing
                                 '[org.eclipse.jetty/jetty-webapp "8.1.0.RC5"])
         ;_ (prn)
         project (update-in project
                            [:repl-options :init]
-                           #(list 'do % (boot-server port mappings handlers)))
+                           #(list 'do % (boot-server (find-webapp-root project)
+                                                     port
+                                                     mappings
+                                                     handlers)))
         ;_ (clojure.pprint/pprint project)
         _ (prn)
         ]
