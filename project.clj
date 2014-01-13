95d56166798bec7e3a218fa0cc95890731a241a7
Andrew Mcveigh <me@andrewmcveigh.com>
2014-01-13 15:15:08 +0100

    lein-release plugin: bumped version from 0.1.6 to 0.1.7-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.andrewmcveigh/lein-boot "0.1.6"
+(defproject com.andrewmcveigh/lein-boot "0.1.7-SNAPSHOT"
   :description "A Leiningen plugin to run ring-servlet with Servlet 3 API."
   :url "http://github.com/andrewmcveigh/lein-boot"
   :license {:name "Eclipse Public License"
